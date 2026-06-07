a30684b3768c5bd5c7f987118a250ea636d27fad
anyelrs-oss <anyelrs@gmail.com>
2026-06-07 19:00:25 -0400

    feat: agregar logo oficial UPTM Digital como icono de la app
    - Icono generado con flutter_launcher_icons v0.14.4
    - Escudo UPTM con circuitos (sin texto) optimizado para app
    - Adaptive icons con fondo #0D1B2A (navy) para Android 8+
    - Legacy icons para Android 7 y anteriores
    - Assets: app_icon.png (icono) + logo_full.jpg (logo completo)
    - Configurado flutter_launcher_icons.yaml

--- uptmdigital_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ uptmdigital_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,18 +5,28 @@
 import FlutterMacOS
 import Foundation
 
+import app_links
+import file_picker
+import file_selector_macos
 import firebase_core
 import firebase_messaging
 import flutter_secure_storage_macos
 import mobile_scanner
 import path_provider_foundation
 import printing
+import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
